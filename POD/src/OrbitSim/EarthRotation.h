94bd55a26fd61ca4a7318ff14e90c0d958d324fe
vdmitriev90 <vm.dmitriev90@gmail.com>
2017-03-17 18:51:52 +0300

    test() added to EarthRotation

--- POD/src/OrbitSim/EarthRotation.h
+++ POD/src/OrbitSim/EarthRotation.h
@@ -14,11 +14,18 @@ namespace POD
         
         EarthRotation();
         EarthRotation(const EOPDataStore & eop);
-        bool loadEOP(const string &  file, EOPDataStore::EOPSource source)
+        bool loadEOP(const string &  file, EOPDataStore::EOPSource source= EOPDataStore::EOPSource::IERS)
             throw(InvalidRequest,FileMissingException);
-        Matrix<double> getJ2k2ECEF(const CommonTime & utc);
-        Matrix<double> getECEF2J2k(const CommonTime & utc);
+        Matrix<double> getJ2k2ECEF(const CommonTime & t);
+        Matrix<double> getECEF2J2k(const CommonTime & t);
+        Vector<double> J2k_2_ECEF(const CommonTime & t, const Vector<double> pos);
+        Vector<double> ECEF_2_J2k(const CommonTime & t, const Vector<double> pos);
+        virtual ~EarthRotation()
+        {
+            eopData.clear();
+        }
 
+        bool test();
     protected:
         EOPDataStore eopData;
 		
